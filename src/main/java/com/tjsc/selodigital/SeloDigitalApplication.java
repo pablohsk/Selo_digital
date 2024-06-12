46fbc9019ac2d3ebd3196a55085e00d79a49643f
Pablohsk <pablofidelisdias@gmail.com>
2024-06-12 19:46:57 -0300

    Criado model para EnteDeclarado

--- src/main/java/com/tjsc/selodigital/SeloDigitalApplication.java
+++ src/main/java/com/tjsc/selodigital/SeloDigitalApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeloDigitalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SeloDigitalApplication.class, args);
     }
-
 }
--- src/main/java/com/tjsc/selodigital/model/EnteDeclarado.java
+++ src/main/java/com/tjsc/selodigital/model/EnteDeclarado.java
@@ -1,4 +1,40 @@
 package com.tjsc.selodigital.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class EnteDeclarado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
