7e1946c0ffd18dbb288317e8808e6bdf2be22fb8
Pablohsk <pablofidelisdias@gmail.com>
2024-06-12 20:06:06 -0300

    Criado código inicial para Service

--- src/main/java/com/tjsc/selodigital/service/EnteDeclaradoService.java
+++ src/main/java/com/tjsc/selodigital/service/EnteDeclaradoService.java
@@ -1,4 +1,23 @@
 package com.tjsc.selodigital.service;
 
+import com.tjsc.selodigital.model.EnteDeclarado;
+import com.tjsc.selodigital.repository.EnteDeclaradoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EnteDeclaradoService {
+
+    @Autowired
+    private EnteDeclaradoRepository enteDeclaradoRepository;
+
+    public List<EnteDeclarado> getAllEntes() {
+        return enteDeclaradoRepository.findAll();
+    }
+
+    public EnteDeclarado createEnte(EnteDeclarado enteDeclarado) {
+        return enteDeclaradoRepository.save(enteDeclarado);
+    }
 }
